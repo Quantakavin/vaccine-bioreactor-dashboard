48ed05382fa089632e5150bb7f89bf22529d9901
Quantakavin <quantakavin@gmail.com>
2024-12-02 12:27:09 +0000

    add form to update readings

--- client/src/pages/Ph.jsx
+++ client/src/pages/Ph.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import Graph from '../components/Graph';
 import config from '../config/config'
@@ -21,11 +23,57 @@ const PhChart = () => {
     return (isPending ? <p>No content to display</p> : <Graph title="Ph" data={data} min={0} max={14} />)
 }
 
+const PhForm = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const mutation = useMutation({
+        mutationFn: async (data) => {
+            return await axios.put(`${config.baseURL}/ph`, data);
+        }
+    })
+
+    const onSubmit = (data) => {
+        mutation.mutate(data, {
+            onSuccess: () => {
+                alert('Ph updated successfully');
+                reset();
+            },
+            onError: (error) => {
+                console.log('Error updating ph:', error);
+                alert('Failed to update ph');
+            },
+        });
+    }
+
+
+    return (
+        <div className="formcontainer">
+            <h4>Update Ph:</h4>
+            <form className="updateform" onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    label="Ph"
+                    type="number"
+                    variant="outlined"
+                    {...register('reading', { required: true })}
+                    inputProps={{
+                        step: 0.01
+                    }}
+                    style={{ marginBottom: '10px'}}
+                />
+                <Button variant="contained" color="primary" type="submit" className="submitbutton">
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
+
 const Ph = () => {
     return (
         <div className="container">
             <h1 className="title">Ph</h1>
             <PhChart />
+            <PhForm />
         </div>
     );
 };
--- client/src/pages/Stirring.jsx
+++ client/src/pages/Stirring.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import Graph from '../components/Graph';
 import config from '../config/config'
@@ -21,11 +23,57 @@ const StirringChart = () => {
     return (isPending ? <p>No content to display</p> : <Graph title="Stirring Speed" data={data} min={0} max={5000}/>)
 }
 
+const StirringForm = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const mutation = useMutation({
+        mutationFn: async (data) => {
+            return await axios.put(`${config.baseURL}/stirring`, data);
+        }
+    })
+
+    const onSubmit = (data) => {
+        mutation.mutate(data, {
+            onSuccess: () => {
+                alert('Stirring Speed updated successfully');
+                reset();
+            },
+            onError: (error) => {
+                console.log('Error updating stirring speed:', error);
+                alert('Failed to update stirring speed');
+            },
+        });
+    }
+
+
+    return (
+        <div className="formcontainer">
+            <h4>Update Stirring Speed:</h4>
+            <form className="updateform" onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    label="Stirring Speed"
+                    type="number"
+                    variant="outlined"
+                    {...register('reading', { required: true })}
+                    inputProps={{
+                        step: 0.01
+                    }}
+                    style={{ marginBottom: '10px'}}
+                />
+                <Button variant="contained" color="primary" type="submit" className="submitbutton">
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
+
 const Stirring = () => {
     return (
         <div className="container">
             <h1 className="title">Stirring Speed</h1>
             <StirringChart />
+            <StirringForm />
         </div>
     );
 };
